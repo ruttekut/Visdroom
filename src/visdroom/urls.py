bdd682808d5c50f1fc3362319d81f4d95fe90d00
Rutger Muller <rutger@Rutgers-MacBook-Pro.local>
2017-10-13 00:04:00 +0200

    Homepage made footer and other sniippets added to templates/snippets.

--- src/visdroom/urls.py
+++ src/visdroom/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from fishstories.views import home
 from django.conf.urls import url, include
 from django.contrib import admin
+from fishstories.views import HomeView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', home)
+    url(r'^', HomeView.as_view(template_name='home/home.html'))
 ]
