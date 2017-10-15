1634737ad2e1025be5d4c4f780516e8418df123d
Rutger Muller <rutger@Rutgers-MacBook-Pro.local>
2017-10-15 16:08:00 +0200

    Added thumbnails for homepage and carousel.

--- src/visdroom/urls.py
+++ src/visdroom/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from fishstories.views import HomeView
+from fishstories.views import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', HomeView.as_view(template_name='home/home.html'))
+    url(r'^', TemplateView.as_view(template_name='home/home.html'))
 ]
